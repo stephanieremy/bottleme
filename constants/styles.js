a7e97ec2c6b3465fe422df768396161fe2e94979
remystephanie <stephanie.remy@bcge.ch>
2024-06-03 09:01:46 +0200

    :lipstick: add colors

---
+++ constants/styles.js
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: "#EEA0BEFF",
+    primary100: "#eea0be",
+    primary200: "#C45282FF",
+    primary400: "#e088ae",
+    primary500: "#b0376d",
+    primary700: "#962357",
+    primary800: "#7f1e4a",
+    accent500: "#f7bc0c",
+    error50: "#fcc4e4",
+    error500: "#9b095c",
+    gray500: "#39324a",
+    gray700: "#221c30",
+  },
+};
